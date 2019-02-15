09188dbacf33ab4b2fa9b396938c2b147ea52af5
manideeppasumarthi <manideep.pasumarthi@dal.ca>
2019-02-15 11:04:26 -0800

    open book quiz

---
+++ task1A.py
@@ -0,0 +1,79 @@
+import mybook=('books1.txt','books2.txt','books3.txt,'r')
+
+
+
+def unique_words()
+	mybook=open('books1.txt')
+	li=[]
+	i=0
+	for line in my book:
+		line=line.split(',')
+		word=line(i)
+	if word not in li:
+		li(word)=1
+	else:
+		li(word)+=1
+	i=i+1
+pint(li)
+
+
+print(unique_words)
+
+
+
+def count_the_article()
+	mynote=open('books2.txt')
+	count=0
+	lis=[]
+	for line in mybook
+		line=line.split()
+
+
+
+
+def sorted_words()
+	mybook=open('book3.txt')
+	while len(line)!=0
+	print(line,end='')
+	line=mybook.readline()
+
+  mybook.close
+
+print(sorted_words)
+
+
+
+def character_word_count()
+    lineCount = 0
+    wordCount = 0
+    charCount = 0
+    list = []
+
+    mybook = raw_input('books1.txt: ')
+    infile = open(mybook, 'r')
+
+    for range in infile:
+        lineCount = lineCount + 1
+        list = string.split(readline, '')
+        wordCount = wordCount + len(list)
+        for ch in file:
+            charCount = charCount + 1
+
+    print "There are", lineCount, "lines in the file."
+    print "There are", charCount, "characters in the file."
+    print "There are", wordCount, "words in the file."
+
+    infile.close()
+
+character_word_count()
+
+
+
+def starts_with_vow()
+	mybook=open('books2.txt','r')
+	vowel=[]
+	for vowel in words:
+    		if vowel [0]=='a,e,i,o,u':
+	    	words.append(vowel)
+	count=dict()words
+    print (count)
